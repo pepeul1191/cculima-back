a5fc666e7c467a62ca621381def785082ce1abbd
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-11 20:52:25 -0500

    Editar teatro

--- routes/teatro.js
+++ routes/teatro.js
@@ -38,4 +38,73 @@ module.exports = [
       });
     }
   },
+  {
+    method: ['POST'],
+    path: 'editar',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function (request, reply) {
+      var data = JSON.parse(request.query.data);
+      var _id = data['id'];
+      models.Teatro.findById(_id, function(err, teatro){
+        if (err){
+          var rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+              'Se ha producido un error en buscar el teatro a editar',
+              err.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }else{
+          if(teatro == null){
+            var rpta = {
+              'tipo_mensaje': 'error',
+              'mensaje': [
+                'Documento a editar no se encuentra en la base de datos'
+              ]
+            }
+            reply(JSON.stringify(rpta));
+          }else{
+            teatro.foto_menu = data.foto_menu;
+            teatro.foto_detalle = data.foto_detalle;
+            teatro.nombre = data.nombre;
+            teatro.titulo = data.titulo;
+            teatro.descripcion = data.descripcion;
+            teatro.elenco = data.elenco;
+            teatro.equipo = data.equipo;
+            teatro.programacion = data.programacion;
+            teatro.comienza = data.comienza;
+            teatro.finaliza = data.finaliza;
+            teatro.organizador = data.organizador;
+            teatro.fechas = data.fechas;
+            teatro.lugar = data.lugar;
+            teatro.save(function (err, updatedDoc) {
+              if (err){
+                var rpta = {
+                  'tipo_mensaje': 'error',
+                  'mensaje': [
+                    'Se ha producido un error en editar el teatro',
+                    err.toString()
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }else{
+                var rpta = {
+                  'tipo_mensaje': 'success',
+                  'mensaje': [
+                    'Se ha editado el teatro',
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }
+            });
+          }
+        }
+      });
+    }
+  },
 ]
--- tests/rspec/teatro.rb
+++ tests/rspec/teatro.rb
@@ -65,4 +65,74 @@ def crear
   end
 end
 
-crear
+def editar
+  RSpec.describe App do
+    describe '2. Editar teatro: ' do
+      it '2.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '2.2 Editar teatro' do
+        teatro = {
+          :id => '5aceaad732e9f229ab9e83b5',
+          :foto_menu => {
+            :url => 'https://www.centroculturalulima.com/wp/wp-content/uploads/2018/03/Banner-y-boton-02.png',
+            :imagen_id => 'lkadjsflkadjsfkladjflas',
+          },
+          :foto_detalle => {
+            :url => 'https://www.centroculturalulima.com/wp/wp-content/uploads/2018/02/Banner-y-boton-01.png',
+            :imagen_id => 'lkadjsflkadjsfkladjflas',
+          },
+          :nombre => 'Deshuesaderosh',
+          :titulo => 'Del 17 de marzo al 25 de junio',
+          :descripcion => 'Esteban Cruz es un joven desempleado que es mantenido por sus padres y que cuenta con evidentes problemas para incorporarse a un mundo que le resulta hostil. Abrumado por su incompetencia para ejercer el más difícil de los oficios: el de ser humano, se percibe en medio de una sociedad pragmática y resultadista en la cual no sabe cómo pertenecer. Una entrevista de trabajo podría cambiar el rumbo de su vida, solo que no en los términos que él esperabash.',
+          :elenco => [
+            'Telmo Arévalo',
+            'Leonor Estrada',
+            'Walter Ramírez',
+            'Rodrigo Rodríguez',
+            'Diego Sakuray 1',
+            'Sammy Zamalloa 3',
+            'Sammy Zamalloa 2',
+          ],
+          :equipo => [
+            'Dramaturgia: Carlos Gonzáles VillanuevaDiseño de arte y vestuario: Ana Cecilia Chung',
+            'Diseño de luces: Jesús Reyes',
+            'Dirección General: Fernando Castro',
+            'Producción: Compañía de Teatro Físico',
+            'Producción: Compañía de Teatro Físico XD',
+          ],
+          :programacion => 'Fecha: Del 17 de mayo al 25 de junio (6 semanas)Días: De Jueves a Lunes <br>Hora: 8:30pm, Domingos (7:00pm)',
+          :comienza => 'mayo 17 @ 8:30 pm',
+          :finaliza => 'junio 25 @ 10:30 pm',
+          :organizador => 'Centro Cultural de la Universidad de Lima',
+          :fechas => [
+            '2016-05-18',
+            '2016-05-19',
+            '2016-05-20',
+            '2016-05-21',
+            '2016-05-22',
+            '2016-05-23',
+            '2016-05-24',
+            '2016-05-25',
+            '2016-05-26',
+          ],
+          :lugar => '5acea95fa181e7288381c640'
+        }
+        url = 'teatro/editar?data=' + teatro.to_json
+        test = App.new(url)
+        test.post()
+        puts test.response.body
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha editado el teatro')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+#crear
+editar
