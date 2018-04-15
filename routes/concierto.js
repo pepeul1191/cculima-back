395611e28997edf1f8dd1aee9d8d9eff7ff6d67c
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-15 09:09:31 -0500

    CRUD de conciertos

--- routes/concierto.js
+++ routes/concierto.js
@@ -1,6 +1,154 @@
 'use strict';
 var models = require('../config/models');
+var async = require('async');
 
 module.exports = [
-
+  {
+    method: ['POST'],
+    path: 'crear',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function (request, reply) {
+      var data = JSON.parse(request.query.data);
+      var concierto = new models.Concierto(
+        data
+      );
+      concierto.save(function (err, createdDoc) {
+        if (err){
+          var rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+              'Se ha producido un error en registrar el concierto',
+              err.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }else{
+          var rpta = {
+            'tipo_mensaje': 'success',
+            'mensaje': [
+              'Se ha registrado el concierto',
+              createdDoc._id.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }
+      });
+    }
+  },
+  {
+    method: ['POST'],
+    path: 'editar',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function (request, reply) {
+      var data = JSON.parse(request.query.data);
+      var _id = data['id'];
+      models.Concierto.findById(_id, function(err, concierto){
+        if (err){
+          var rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+              'Se ha producido un error en buscar el concierto a editar',
+              err.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }else{
+          if(concierto == null){
+            var rpta = {
+              'tipo_mensaje': 'error',
+              'mensaje': [
+                'Documento a editar no se encuentra en la base de datos'
+              ]
+            }
+            reply(JSON.stringify(rpta));
+          }else{
+            concierto.nombre = data.nombre;
+            concierto.titulo = data.titulo;
+            concierto.descripcion = data.descripcion;
+            concierto.comienza = data.comienza;
+            concierto.finaliza = data.finaliza;
+            concierto.organizador = data.organizador;
+            concierto.fechas = data.fechas;
+            concierto.lugar = data.lugar;
+            concierto.direccion = data.direccion;
+            concierto.foto_menu = data.foto_menu;
+            concierto.foto_detalle = data.foto_detalle;
+            concierto.save(function (err, updatedDoc) {
+              if (err){
+                var rpta = {
+                  'tipo_mensaje': 'error',
+                  'mensaje': [
+                    'Se ha producido un error en editar el concierto',
+                    err.toString()
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }else{
+                var rpta = {
+                  'tipo_mensaje': 'success',
+                  'mensaje': [
+                    'Se ha editado el concierto',
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }
+            });
+          }
+        }
+      });
+    }
+  },
+  {
+    method: ['POST'],
+    path: 'guardar',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function (request, reply) {
+      var data = JSON.parse(request.query.data);
+      var eliminados = data['eliminados'];
+      var _id = request.query._id;
+      var error = false;
+      var tests = [];
+      async.each(eliminados, function(eliminado_id, callback){
+        models.Concierto.findByIdAndRemove(eliminado_id, function(err, doc){
+          if (err){
+            callback(err);
+            return;
+          }else{
+            callback();
+          }
+        });
+      }, function(err){
+        if(err){
+          var rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+              'Se ha producido un error en eliminar los conciertos',
+              err.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }else{
+          var rpta = {
+            'tipo_mensaje': 'success',
+            'mensaje': [
+              'Se ha registrado los cambios en los conciertos',
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }
+      });
+    }
+  },
 ];
