377f985e8a1906fd33868e0735ce6fbae1ef1727
Pepe Valdivia <jvaldivia@softweb.pe>
2018-04-25 18:55:28 -0500

    Editar servicio

--- routes/servicio.js
+++ routes/servicio.js
@@ -441,4 +441,65 @@ module.exports = [
       });
     }
   },
+  {
+    method: ['POST'],
+    path: 'editar_detalle',
+    config: {
+      auth: false,
+      pre: [
+      ],
+    },
+    handler: function (request, reply) {
+      var data = JSON.parse(request.query.data);
+      var _id = data['_id'];
+      models.Servicio.findById(_id, function(err, servicio){
+        if (err){
+          var rpta = {
+            'tipo_mensaje': 'error',
+            'mensaje': [
+              'Se ha producido un error en buscar el servicio a editar',
+              err.toString()
+            ]
+          }
+          reply(JSON.stringify(rpta));
+        }else{
+          if(servicio == null){
+            var rpta = {
+              'tipo_mensaje': 'error',
+              'mensaje': [
+                'Documento a editar no se encuentra en la base de datos'
+              ]
+            }
+            reply(JSON.stringify(rpta));
+          }else{
+            servicio.titulo = data.titulo;
+            servicio.descripcion = data.descripcion;
+            servicio.latitud = data.latitud;
+            servicio.longitud = data.longitud;
+            servicio.direccion = data.direccion;
+            servicio.save(function (err, updatedDoc) {
+              if (err){
+                var rpta = {
+                  'tipo_mensaje': 'error',
+                  'mensaje': [
+                    'Se ha producido un error en editar el servicio',
+                    err.toString()
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }else{
+                var rpta = {
+                  'tipo_mensaje': 'success',
+                  'mensaje': [
+                    'Se ha editado el servicio',
+                  ]
+                }
+                reply(JSON.stringify(rpta));
+              }
+            });
+          }
+        }
+      });
+    }
+  },
 ];
